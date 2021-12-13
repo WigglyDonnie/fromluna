865aae11e582922bf0097bb1b76c97f4500a5d57
donnie <donfather1000@gmail.com>
2021-12-13 18:52:55 -0500

    implement basic search

--- src/components/LoginMenu.tsx
+++ src/components/LoginMenu.tsx
@@ -5,11 +5,17 @@ function LoginMenu() {
   const [name, setName] = useState("");
 
   return (
-    <div className="LoginMenu">
+    <div >
+
+      <div className="LoginMenu">
       <button onClick={() => setAuthed(true)}> Login </button>
       <button onClick={() => setAuthed(false)}> Logout </button>
-
       <div>{authed ? <div>IM A NAME</div> : <div>IM NOT A NAME</div>}</div>
+      </div>
+
+      
+
+      
     </div>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Brand from './Brand';
 import LoginMenu from './LoginMenu'
 
 function Nav() {
+
+  const [formValue, setFormValue] = useState("");
+
   return (
     <div className="Nav">
       <Brand />
       <LoginMenu />
+      <input onChange={(event) => {console.log(event.target.value)}} type="text" name="name" autoComplete="off"/>
     </div>
   );
 }
