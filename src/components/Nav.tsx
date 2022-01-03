b4f9acc77d4c06895c996d0c081746ce060154d3
donnie <donfather1000@gmail.com>
2022-01-02 22:22:25 -0500

    change navbar styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,30 +2,18 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import './Nav.scss'
 
 export default function Nav() {
   return (
-    <Box className='Nav' sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sleep & Rest</Button>
+          <Button color="inherit">Eat</Button>
+          <Button color="inherit">Play</Button>
+          <Button color="inherit">Add</Button>
         </Toolbar>
       </AppBar>
     </Box>
