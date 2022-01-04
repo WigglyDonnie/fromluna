2cd5db2d54bb0aa47db25b9b0e3f3d0e728a61fb
donnie <donfather1000@gmail.com>
2022-01-04 18:51:34 -0500

    restructure search container

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from './components/Nav';
 import Head from './components/Head';
 import LoginBar from './components/LoginBar';
-import SearchContainer from './components/SearchContainer';
+import SearchContainer from './components/SearchContainer/SearchContainer';
 
 
 
