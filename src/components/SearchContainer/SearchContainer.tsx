de8ca46073bc3a9aae564a882573d0e123070c02
donnie <donfather1000@gmail.com>
2022-01-04 19:36:00 -0500

    work on searchbar logic

--- src/components/SearchContainer/SearchContainer.tsx
+++ src/components/SearchContainer/SearchContainer.tsx
@@ -1,19 +1,14 @@
 import * as React from "react";
 
 import './SearchContainer.scss'
-import SearchBox from "./SearchBox";
 import {mapSearch} from "./mapSearch"
 
 
 export default function SearchContainer() {
 
-  console.log(mapSearch(['bing', 'bong', 'bo', 'bing', 'bong', 'bo']));
   return (
   <div className='SearchContainer'>
-    <SearchBox/>
-    <SearchBox/>
-    <SearchBox/>
-    <SearchBox/>
+    {mapSearch(['bing', 'bong', 'bo'])}
   </div>
 
   );
