ac59b02188c710152b3ce3b2f8b9187fda1f6c95
donnie <donfather1000@gmail.com>
2022-01-05 07:39:50 -0500

    make sure searchboxes can dynamically update correctly

--- src/components/SearchContainer/SearchContainer.tsx
+++ src/components/SearchContainer/SearchContainer.tsx
@@ -16,16 +16,10 @@ export default function SearchContainer() {
 
 const mapSearch = (query:string[]) => {
   let searchQuery: string[] = query;
-  let counter = 0;
-
-  let results = <SearchBox/>
+  let results = []
 
   for (let i = 0; i < searchQuery.length; i++) {
-    counter ++;
-  }
-
-  for (let i = 0; i < counter; i++) {
-    
+    results.push(<SearchBox key={i}/>);
   }
   
   return results;
