b6cdcbec1c659c3ae0299ed591c9720c5ea100b3
donnie <donfather1000@gmail.com>
2022-01-05 07:32:00 -0500

    try to get searchbox to dynamically render

--- src/components/SearchContainer/SearchContainer.tsx
+++ src/components/SearchContainer/SearchContainer.tsx
@@ -1,15 +1,32 @@
 import * as React from "react";
 
 import './SearchContainer.scss'
-import {mapSearch} from "./mapSearch"
 
+import SearchBox from "./SearchBox";
 
 export default function SearchContainer() {
 
   return (
   <div className='SearchContainer'>
-    {mapSearch(['bing', 'bong', 'bo'])}
+  {mapSearch(['bing', 'bang', 'bong'])}
   </div>
 
   );
+};
+
+const mapSearch = (query:string[]) => {
+  let searchQuery: string[] = query;
+  let counter = 0;
+
+  let results = <SearchBox/>
+
+  for (let i = 0; i < searchQuery.length; i++) {
+    counter ++;
+  }
+
+  for (let i = 0; i < counter; i++) {
+    
+  }
+  
+  return results;
 };
